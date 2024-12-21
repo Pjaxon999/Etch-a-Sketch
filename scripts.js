1a67b81f9cf3bcbecc29bbe7df079adc8c30e172
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2024-12-21 10:47:57 -0700

    Changed Function for grid making added structure
    Changed the function to create a 16x16 grid, creates evenly spaced boxes
    regardless of container width by using flex basis to define the sizing.
    Should be able to refactor later to accommodate changing values. Also
    added some CSS properties to help the container stand out and created a
    new div to house the main content so that I can create a more appealing
    layout in the future.

--- scripts.js
+++ scripts.js
@@ -6,8 +6,9 @@ const container = document.querySelector(".container");
 
 // Create a 16x16 grid of divs inside the "container" div
 function gridMaker() {
-    for (let i =0; i < 16; i++) {
+    for (let i =0; i < 256; i++) {
         let newDiv = document.createElement("div");
+        newDiv.setAttribute("style", "color: #D9D9D9; flex: 0 0 6.25%")
         container.appendChild(newDiv);
     }
 } 
