74be8037a708f624e6f3af46f41122f2a95dcf60
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2024-12-21 08:54:13 -0700

    Added basic framework for creating the draw grid
    Added a function that creates 16 divs inside of a container div via DOM
    manipulation. This works by using a for loop to create the divs and
    append them to the parent node of "container". From here, I can add
    some CSS to make it look more like a grid and be well on my way towards
    something resmebling an actual Etch-a-Sketch.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,13 @@
 // Testing External JS link
 console.log("Hi there! This is a test!");
+
+// Targeting container div for later use
+const container = document.querySelector(".container");
+
+// Create a 16x16 grid of divs inside the "container" div
+function gridMaker() {
+    for (let i =0; i < 16; i++) {
+        let newDiv = document.createElement("div");
+        container.appendChild(newDiv);
+    }
+} 
