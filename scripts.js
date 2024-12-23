772b5b3e70f11f8696100d717b67c5e0c0563507
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2024-12-23 09:38:46 -0700

    Added functionality for changing the canvas
    Added functions as outlined in the project file. newArea() will take a
    user input, make sure that is valid, then use that value as the new
    number of squares per side in the canvas, update the area and percentage
    used so that the flex basis can be updated to make sure that the new
    divs take up the appropriate space. It then clears the canvas and runs
    gridMaker() again to generate the new canvas. I also added a shake()
    function, which keeps the canvas size and background the same but resets
    the background colors of the individual divs, mimicking the behavior of
    a real life Etch-a-Sketch.
    
    I will now be adding buttons to trigger these functions. After that, I
    will begin looking into how to do add some of the optional features(
    random color mode, opacity) from the project instructions.

--- scripts.js
+++ scripts.js
@@ -28,26 +28,57 @@ let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
-// Create a 16x16 grid of divs inside the "container" div
+// Set variables for user requested grid
+let gridArea = 256;
+let gridAreaPercentage = 6.25;
+
+// function for button to change grid area
+function newArea() {
+    let userArea = prompt("How many squares per side would you like in your new canvas? 1-100 please!");
+    userArea = userArea.trim();
+    userArea = parseInt(userArea);
+    if (userArea === null || userArea < 1 || userArea > 100 || !Number.isInteger(userArea)|| !Number.isFinite(userArea) || isNaN(userArea)) {
+        alert("Something went wrong. Try again with a number between 1-100");
+    } else {
+        gridArea = userArea * userArea;
+        gridAreaPercentage = 100 / userArea;
+        console.log(gridAreaPercentage)
+        clear();
+        gridMaker();
+    }
+};
+
+// Create a grid of divs inside the "container" div
 function gridMaker() {
-    for (let i =0; i < 256; i++) {
+    for (let i = 0; i < gridArea; i++) {
         let newDiv = document.createElement("div");
-        newDiv.setAttribute("style", "background-color: #D9D9D9; flex: 0 0 6.25%");
+        newDiv.setAttribute("style", `background-color: #D9D9D9; flex: 0 0 ${gridAreaPercentage}%`);
+        newDiv.setAttribute("class", "canvas")
         newDiv.addEventListener('mouseover', draw);
         newDiv.addEventListener('mousedown', draw);
         container.appendChild(newDiv);
     }
-} 
+}; 
 
+// Main drawing logic
 function draw(e) {
     let drawDiv = e.currentTarget; 
-    if (mouseDown == true) {
-        drawDiv.style.backgroundColor = "black";
-    } else if (e.type == "mousedown") {
+    if (mouseDown == true || e.type == "mousedown") {
         drawDiv.style.backgroundColor = "black";
     }
+};
 
-}
+// Clears canvas before creating a new one
+function clear() {
+    container.innerHTML = '';
+};
 
+// Clears canvas but maintains current div size
+function shake() {
+    let canvasArea = document.querySelectorAll(".canvas");
+    canvasArea.forEach(div => {
+        div.style.backgroundColor = "#D9D9D9";
+    });
+};
 
 gridMaker();
