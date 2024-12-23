63325c5ee679aed3fddd32a868434dce6f9a03bd
Pjaxon999 <180225649+Pjaxon999@users.noreply.github.com>
2024-12-23 07:57:51 -0700

    Changed draw logic, prevented dragging problems
    When the original draw logic was implemented, on occasion the default
    drag behavior was causing problems with drawing. I changed the logic
    slightly to trigger based on the event, and also disabled drag behaviors
    globally (via dragstart, disabling defaults for relevant mouse events,
    and user-select: none).

--- scripts.js
+++ scripts.js
@@ -4,11 +4,50 @@ console.log("Hi there! This is a test!");
 // Targeting container div for later use
 const container = document.querySelector(".container");
 
+// Disabling certain events globally to prevent problems with dragging and drawing
+document.addEventListener('dragstart', (e) => {
+    e.preventDefault();
+});
+document.addEventListener('mousedown', (e) => {
+    e.preventDefault();
+});
+document.addEventListener('mousemove', (e) => {
+    if (e.buttons == 1) {
+        e.preventDefault();
+    }
+});
+document.addEventListener('mmouseup', (e) => {
+    e.preventDefault();
+});
+window.addEventListener('selectstart', (e) => {
+    e.preventDefault
+});
+
+// Track mouse being held down 
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
 // Create a 16x16 grid of divs inside the "container" div
 function gridMaker() {
     for (let i =0; i < 256; i++) {
         let newDiv = document.createElement("div");
-        newDiv.setAttribute("style", "background-color: #D9D9D9; flex: 0 0 6.25%")
+        newDiv.setAttribute("style", "background-color: #D9D9D9; flex: 0 0 6.25%");
+        newDiv.addEventListener('mouseover', draw);
+        newDiv.addEventListener('mousedown', draw);
         container.appendChild(newDiv);
     }
 } 
+
+function draw(e) {
+    let drawDiv = e.currentTarget; 
+    if (mouseDown == true) {
+        drawDiv.style.backgroundColor = "black";
+    } else if (e.type == "mousedown") {
+        drawDiv.style.backgroundColor = "black";
+    }
+
+}
+
+
+gridMaker();
